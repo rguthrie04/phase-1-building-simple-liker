d6c3fed30aa2a96753bad3146141ca951aeb3baa
Ryan <ryanguthrie@zoho.com>
2021-09-29 12:33:27 +0100

    commit tree 76c3bf955da253df9e16dd83fa8ffa5a0c296478

--- main.js
+++ main.js
@@ -3,10 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartIcon = document.getElementsByClassName("like-glyph")
 
 
 
 
+function likeMe(e){
+  const heart = e.target
+  mimicServerCall('url')
+    .then(function(){
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART
+        heart.className = 'activated-heart'
+      } else{
+        heart.innerText = EMPTY_HEART
+        heart.className = ""
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal")
+      modal.className = ""
+      modal.innerText = 'error'
+      setTimeout(() => modal.className = "hidden",3000)
+    })
+
+}
+for (const folks of heartIcon){
+  folks.addEventListener('click',likeMe)
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
